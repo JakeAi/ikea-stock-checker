9da861b702b916eac4964418bd6ec0750247b106
Jake Ihasz <JakeIhasz@minnich-mfg.com>
2021-01-14 15:40:23 -0500

    update

--- apps/ikea-stock-site/src/app/app.component.ts
+++ apps/ikea-stock-site/src/app/app.component.ts
@@ -71,7 +71,7 @@ export class AppComponent {
     this.store.length = 0;
 
     let m;
-    const regex = /([0-9]{6,})\s*([^\$]+)\$([^\n]+)\s*(\d+)\s*\$([^\n]+)/gmi;
+    const regex = /\s*(\d+)\s*([^\$]+)\$([^\s]+)\s*([^\s]+)\s*\$([^\s]+)/gm;
 
     while ((m = regex.exec(this.skuList)) !== null) {
       if (m.index === regex.lastIndex) { regex.lastIndex++; }
--- apps/ikea-stock-site/src/app/app.module.ts
+++ apps/ikea-stock-site/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2DeepSearchPipe } from './search.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,Ng2DeepSearchPipe],
   imports: [BrowserModule, HttpClientModule, FormsModule, Ng2SearchPipeModule],
   providers: [],
   bootstrap: [AppComponent]
